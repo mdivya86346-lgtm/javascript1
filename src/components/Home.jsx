0f232189fd182475d0edbd0f43f920216fe5d9ad
divya <mdivya86346@gmail.com>
2026-06-25 11:21:23 +0530

    june25

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -16,11 +16,36 @@
         </>
     )
 }*/
-function Home( {name="user" ,age="no age"}){
+// function Home( {name="user" ,age="no age"}){
+//     return (
+//         <>
+//         <h1> she is{name}and she is{age}</h1>
+//         </>
+//     )
+// }
+// export default Home
+
+import { useState } from "react"
+export default function Home(){
+    const [count, setCount] = useState(0)
+    const [value, setValue] = useState(0)
+
+
+   // const [count,setCount] = useState(0)
+    const incCount = () => {
+        setCount((prev) => prev + 1)
+        const deCount = () =>
+        {
+            if(count>0){
+            setCount((prev) => prev - 1)
+            }
+        }
+          
+    }
     return (
         <>
-        <h1> she is{name}and she is{age}</h1>
+        <h1>{count}</h1>
+        <button onClick={incCount}>+</button>
         </>
     )
 }
-export default Home
